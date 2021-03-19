2b1115bcc4b46d046c9ca001be50cb0b2899e7d3
sbriones97 <sbriones97@hotmail.com>
2021-03-18 18:03:22 -0600

    Solo falta el dockerfile, crear la imagen y subirla

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,28 +1,47 @@
-const fetch = require('node-fetch')
 
-let boton = document.getElementById('search_news').addEventListener('click', analizarTexto);
+let boton = document.getElementById('analyze_texto').addEventListener('click', analizarTexto);
 let url = 'https://tone-analyzer-salvador-briones.mybluemix.net/peticion'
 
+
 function analizarTexto(){
-    let texto = document.getElementById('search_text').value
+    let texto = document.getElementById('text_to_send').value
     let body = { 'text': texto }
     let metodo = {
             method: 'post',
-            body:    JSON.stringify(body),
+            body: JSON.stringify(body),
             headers: { 'Content-Type': 'application/json' },
         }
 
-    fetch(url, )
-        .then(response => {
-            if(response.status == 200) {
-                return response.text()
-            }
-        } )
-        .then(body => {
-            res.status(200).end(body)
-        })
-        .catch(err => {
-            res.status(err.status).end(err.message);
-        });
+
+    const Http = new XMLHttpRequest()
+    Http.open("POST", url);
+    Http.setRequestHeader('Content-Type', 'application/json');
+    Http.send(JSON.stringify(body));
+    
+    Http.onreadystatechange = (e) => {
+        document.getElementById("aqui_mero").innerHTML = Http.responseText
+        console.log(Http.responseText)
+        // if(Http.responseText.currentTarget.status == 200){
+        //     document.getElementById('aqui_mero').value = Http.responseText
+        //     console.log(Http.responseText)
+        // }
+        // else {
+        //     document.getElementById('aqui_mero').value = Http.responseText
+        //     console.log(e)
+        // }
+    }
+
+    // fetch(url, metodo)
+    //     .then(response => {
+    //         if(response.status == 200) {
+    //             return response.text()
+    //         }
+    //     } )
+    //     .then(body => {
+    //         console.log(body)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     });
 
 }
